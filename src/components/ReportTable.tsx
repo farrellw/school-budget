7923e0255da248a348595bcd986cb626f7cdfdd2
Will Farrell <wfarrell@1904labs.com>
2019-11-18 12:34:12 -0600

    docs.

--- src/components/ReportTable.tsx
+++ src/components/ReportTable.tsx
@@ -1,26 +1,69 @@
 import * as React from "react";
-import * as exampleForJSON from "../data/exampleForForm.json";
-
+import { useState } from "react";
+import * as savedSchoolsJSON from "../data/savedSchools.json";
 
 function ReportTable() {
+    const [selected, setSelected] = useState("HighSchools ( All )");
     // Use the state and functions returned from useTable to build your UI
-    
+
+    const setSelection = (event: React.FormEvent<HTMLSelectElement>) => {
+        setSelected(event.currentTarget.value);
+    }
+
+    const formData = (savedSchoolsJSON.find((node) => {
+        return node.name === selected;
+    }) || savedSchoolsJSON[0])["non-grant-operating-budget"];
+
     // Render the UI for your table
     return (
-        <table >
-            <tr>
-                <th>Field Name</th>
-                <th>Value</th>
-            </tr>
-            { exampleForJSON["non-grant-operating-budget"].map((n, i: number) => {
-                return (
-                    <tr key={i}>
-                        <td>{n.field}</td>
-                        <td>{n.value}</td>
+        <section className="table">
+            <select onChange={setSelection}>
+                {savedSchoolsJSON.map((n, i: number) => {
+                    return (
+                        <option key={i} value={n.name} >{n.name}</option>
+                    )
+                })}
+            </select><br />
+            <caption>{selected}</caption>
+            <table >
+                <thead>
+                    <tr>
+                        <th>Field Name</th>
+                        <th>Value</th>
                     </tr>
-                )
-            })}
-        </table>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Administrative Salaries</td>
+                        <td>{formData["Administrative Salaries"]}</td>
+                    </tr>
+                    <tr>
+                        <td>Instructional Salaries</td>
+                        <td>{formData["Instructional Salaries"]}</td>
+                    </tr>
+                    <tr>
+                        <td>Instructional Support Salaries</td>
+                        <td>{formData["Instructional Support Salaries"]}</td>
+                    </tr>
+                    <tr>
+                        <td>Non-Instructional Support Salaries</td>
+                        <td>{formData["Non-Instructional Support Salaries"]}</td>
+                    </tr>
+                    <tr>
+                        <td>Temp/Part-Time/Sub</td>
+                        <td>{formData["Temp/Part-Time/Sub"]}</td>
+                    </tr>
+                    <tr>
+                        <td>Benefits</td>
+                        <td>{formData.Benefits}</td>
+                    </tr>
+                    <tr>
+                        <td>Discretionary Budget</td>
+                        <td>{formData["Discretionary Budget"]}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </section>
     )
 }
 
