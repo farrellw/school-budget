d8a514775c35f678f78a51a7b540b0560946658f
farrellw <farrell.will.g@gmail.com>
2019-12-09 21:01:27 -0600

    Add body component.

---
+++ src/lib/Body.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+// import { useState } from "react";
+
+
+function Body() {
+  return (
+    <section>
+        <div>
+            <p>Table here</p>
+        </div>
+        <div>
+            <p>Chart here</p>
+        </div>
+    </section>
+  )
+}
+
+export default Body;
