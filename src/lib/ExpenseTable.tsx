2e298513e02bc88cc975247538d7490249be50cc
farrellw <farrell.will.g@gmail.com>
2019-12-10 22:29:15 -0600

    Add expense bar chart.

--- src/lib/ExpenseTable.tsx
+++ src/lib/ExpenseTable.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import { ITableData, SchoolExpense } from "../models/Data";
+import * as Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
 
 interface IProps {
   selectedSchools: SchoolExpense[];
@@ -13,17 +15,73 @@ function ExpenseTable({ selectedSchools, headers, rows, caption }: IProps) {
     return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   };
 
+  const options: Highcharts.Options = {
+    chart: {
+      type: "bar"
+    },
+    title: {
+      text: caption
+    },
+    xAxis: {
+      categories: rows.map(n => n.label)
+    },
+    yAxis: {
+      min: 0,
+      title: {
+        text: "Dollars",
+        align: "high"
+      },
+      labels: {
+        overflow: "justify"
+      }
+    },
+    plotOptions: {
+      bar: {
+        dataLabels: {
+          enabled: true
+        }
+      }
+    },
+    legend: {
+      layout: "vertical",
+      align: "right",
+      verticalAlign: "top",
+      x: -40,
+      y: 80,
+      floating: true,
+      borderWidth: 1,
+      backgroundColor:
+        (Highcharts.defaultOptions.legend &&
+          Highcharts.defaultOptions.legend.backgroundColor) ||
+        "#FFFFFF",
+      shadow: true
+    },
+    credits: {
+      enabled: false
+    },
+    series: selectedSchools.map((s,i) => {
+      return {
+        type: "bar",
+        name: headers[i + 1],
+        data: rows.map(r => {
+          return s[r.key];
+        }
+        )
+    }}) 
+  };
+
+  // tslint:disable-next-line:no-debugger
+  debugger;
+
   return (
     <section className="expense-section">
       <table>
         <caption>{caption}</caption>
         <thead>
           <tr>
-            {
-              headers.map((n, i) => {
-                return <th key={i}>{n}</th>
-              })
-            }
+            {headers.map((n, i) => {
+              return <th key={i}>{n}</th>;
+            })}
           </tr>
         </thead>
         <tbody>
@@ -40,7 +98,7 @@ function ExpenseTable({ selectedSchools, headers, rows, caption }: IProps) {
         </tbody>
       </table>
       <div>
-        <p>Chart here</p>
+        <HighchartsReact highcharts={Highcharts} options={options} />
       </div>
     </section>
   );
