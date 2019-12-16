bea52463c8d5c6cb19997e5d5fa64045d5fb19d3
Abadi Kurniawan <abadi.kurniawan@gmail.com>
2019-12-16 00:42:36 -0600

    Replace react-autosuggest with react-select

--- src/lib/Search.tsx
+++ src/lib/Search.tsx
@@ -1,56 +1,27 @@
 import * as React from "react";
-import Autosuggest from "react-autosuggest";
+import Select from "react-select";
 import schools from "../data/SchoolExpenses.json";
 import { IGeneralSchoolExpense } from "src/models/Data.js";
 
-// Teach Autosuggest how to calculate suggestions for any given input value.
-const getSuggestions = (value: string) => {
-  const inputValue = value.trim().toLowerCase();
-  const inputLength = inputValue.length;
+interface IOption {
+  value: string;
+  label: string;
+}
+const options: IOption[] = schools.map((school: IGeneralSchoolExpense) => ({
+  value: school.id,
+  label: school.name
+}));
 
-  return inputLength === 0
-    ? []
-    : schools.filter(school => school.name.toLowerCase().indexOf(value) !== -1);
-};
-
-const getSuggestionValue = (suggestion: IGeneralSchoolExpense) =>
-  suggestion.name;
-
-const renderSuggestion = (suggestion: IGeneralSchoolExpense) => (
-  <div>{suggestion.name}</div>
-);
+function selectSchool(option: IOption) {
+  // TODO: use react router instead.
+  window.location.href = `/?id=${option.value}`;
+}
 
 function Search() {
-  const [suggestions, setSuggestions] = React.useState<IGeneralSchoolExpense[]>(
-    []
-  );
-
-  const [value, setValue] = React.useState("");
-
-  const inputProps = {
-    placeholder: "Search for school",
-    value,
-    onChange: (event: React.FormEvent, { newValue }: { newValue: string }) =>
-      setValue(newValue)
-  };
-
-  const onSuggestionsFetchRequested = ({ value: val }: { value: string }) => {
-    setSuggestions(getSuggestions(val));
-  };
-
-  const onSuggestionsClearRequested = () => {
-    setSuggestions([]);
-  };
-
   return (
-    <Autosuggest
-      suggestions={suggestions}
-      onSuggestionsFetchRequested={onSuggestionsFetchRequested}
-      onSuggestionsClearRequested={onSuggestionsClearRequested}
-      getSuggestionValue={getSuggestionValue}
-      renderSuggestion={renderSuggestion}
-      inputProps={inputProps}
-    />
+    <>
+      <Select options={options} onChange={selectSchool} />
+    </>
   );
 }
 
