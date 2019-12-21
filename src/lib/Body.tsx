236f2c05dcccbe0dd67fd54b97203e264fc8244f
farrellw <farrell.will.g@gmail.com>
2019-12-21 16:31:13 -0600

    Move view options into its own component.

--- src/lib/Body.tsx
+++ src/lib/Body.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 import { useState } from "react";
 import { TotalOrPerStudent } from "../models/Data";
-import Switch from "react-switch";
 import { useLocation } from "react-router-dom";
 import Expenses from "./Expenses";
+import ViewOptions from './ViewOptions';
 
 function useQuery() {
   return new URLSearchParams(useLocation().search);
@@ -15,7 +15,7 @@ function Body() {
   const query = useQuery();
   const selectedIds: string[] = query.getAll("id");
 
-  const handleChange = () => {
+  const handleToggleChange = () => {
     if (toggle === "Total") {
       setToggle("Per Student");
     } else {
@@ -29,27 +29,7 @@ function Body() {
 
   return (
     <section className="body">
-      <div className="displayOptions">
-        <label>
-          <span>Total</span>
-          <Switch
-            onChange={handleChange}
-            checked={toggle === "Per Student"}
-            uncheckedIcon={false}
-            checkedIcon={false}
-            offColor={"#34baeb"}
-            onColor={"#a2eb34"}
-          />
-          <span>Per Student</span>
-        </label>
-        <label>
-          <Switch
-            onChange={handleCompareWithAverageChange}
-            checked={compareWithAverage}
-          />
-          <span>Compare Against Average</span>
-        </label>
-      </div>
+      <ViewOptions onCompareWithAverageChange={handleCompareWithAverageChange} onToggleChange={handleToggleChange} toggle={toggle} compareWithAverage={compareWithAverage}/>
       <Expenses selectedIds={selectedIds} compareWithAverage={compareWithAverage} toggle={toggle}/>
     </section>
   );
