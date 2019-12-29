f3ca62247ec1a7d62ee8239d963b2497684eeb60
Abadi Kurniawan <abadi.kurniawan@gmail.com>
2019-12-29 15:17:01 -0600

    Switch to classless component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-import * as React from "react";
+import React from "react";
 import Body from "./lib/Body";
 import Header from "./lib/Header";
 import "./App.scss";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-class App extends React.Component {
-  public render() {
-    return (
-      <Router>
-        <Route path="*">
-          <section>
-            <Header />
-            <Body />
-          </section>
-        </Route>
-      </Router>
-    );
-  }
+function App() {
+  return (
+    <Router>
+      <Route path="*">
+        <section>
+          <Header schools={[]} />
+          <Body />
+        </section>
+      </Route>
+    </Router>
+  );
 }
 
 export default App;
--- src/lib/Header.tsx
+++ src/lib/Header.tsx
@@ -1,13 +1,15 @@
 import * as React from "react";
 import Search from "./Search";
 import "./Header.scss";
+import { IGeneralSchoolExpense } from "src/models/Data";
 
-function Header() {
+type Props = { schools: IGeneralSchoolExpense[] };
+function Header({ schools }: Props) {
   return (
     <header className="sb-header">
       <h1 className="sb-heading1">SLPS Budget</h1>
       <nav></nav>
-      <Search />
+      {schools.length > 0 && <Search />}
     </header>
   );
 }
