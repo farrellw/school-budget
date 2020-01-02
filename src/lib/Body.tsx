6ee64760042789f319e1419ece4081b5cd0bb96a
farrellw <farrell.will.g@gmail.com>
2020-01-02 15:39:45 -0600

    Remove expenses component.

--- src/lib/Body.tsx
+++ src/lib/Body.tsx
@@ -1,17 +1,40 @@
 import * as React from "react";
-import Expenses from "./Expenses";
+import { useState } from "react";
 import {
   ISchool
 } from "../models/Data";
 import SchoolInformationSlider from "./SchoolInformationSlider";
+import GeneralExpense from "./GeneralExpense";
+import CategoryExpense from "./CategoryExpense";
 
 type Props = { schools: ISchool[] };
 function Body({ schools }: Props) {
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const categoryClickEvent = (
+    event: React.MouseEvent<HTMLTableRowElement, MouseEvent>
+  ): void => {
+    setSelectedCategory(event.currentTarget.id);
+  };
 
   return (
     <main className="body">
       <SchoolInformationSlider schools={schools} />
-      <Expenses schools={schools} />
+      <div className="expense-panel">
+        <GeneralExpense
+          schools={schools}
+          categoryClickHandler={categoryClickEvent}
+          category={selectedCategory}
+        />
+      </div>
+      <div className="expense-panel">
+        {selectedCategory && selectedCategory !== "" && (
+          <CategoryExpense
+          category={selectedCategory}
+            schools={schools}
+          />
+        )}
+      </div>
     </main>
   );
 }
